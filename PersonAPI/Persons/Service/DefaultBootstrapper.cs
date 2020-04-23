58d54a0f3d89c016c84741512ea484a28a0e0e26
e.zholobov <e.zholobov@bright-soft.ru>
2020-04-24 03:35:33 +0500

    Добавлен DI

---
+++ PersonAPI/Persons/Service/DefaultBootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Nancy;
+using Nancy.TinyIoc;
+using Persons.Abstractions;
+
+namespace Persons.Service
+{
+    public class DefaultBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override IRootPathProvider RootPathProvider => new DefaultRootPathProvider();
+
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            if (container == null) 
+                return;
+
+            container.Register<ICommandHandler<CreatePersonCommand, Guid>, CreatePersonCommandHandler>().AsMultiInstance();
+            container.Register<IQueryHandler<GetPersonQuery, PersonDto>, GetPersonQueryHandler>().AsMultiInstance();
+            container.Register<IPersonRepository, PersonRepository>().AsSingleton();
+        }
+    }
+}
