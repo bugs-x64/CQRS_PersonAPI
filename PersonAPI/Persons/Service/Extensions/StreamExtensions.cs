987a7d88ff02c53fa3ba433ea1da71bdfa2b3ef7
e.zholobov <e.zholobov@bright-soft.ru>
2020-04-25 23:50:47 +0500

    рефакторинг

---
+++ PersonAPI/Persons/Service/Extensions/StreamExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Persons.Service.Extensions
+{
+    public static class StreamExtensions
+    {
+        /// <summary>
+        /// Читает поток в строку.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static string ReadToString(this Stream stream)
+        {
+            if (stream is null)
+                throw new ArgumentNullException(nameof(stream));
+
+            var length = stream.Length;
+            var data = new byte[length];
+
+            stream.Read(data, 0, (int)length);
+
+            stream.Position = 0;
+
+            return System.Text.Encoding.Default.GetString(data);
+        }
+    }
+}
