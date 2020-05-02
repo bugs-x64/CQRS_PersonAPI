e7b7b1328008a19bb6242c59b4035ff814b1da4b
Evsey Zholobov <evsei124@gmail.com>
2020-05-02 20:52:42 +0500

    GH-3 фикс ресурсов

--- PersonAPI/Persons/Resources.Designer.cs
+++ PersonAPI/Persons/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Persons {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Persons.Service.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Persons.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
