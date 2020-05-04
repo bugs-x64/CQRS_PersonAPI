07535b3314416e5e47c8d4d4fe951fa588c75a06
Evsey Zholobov <evsei124@gmail.com>
2020-05-04 22:27:45 +0500

    GH-20 add modules integration tests

---
+++ PersonAPI/Persons.IntegrationTests/Modules/PersonsCommandsModuleTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Autofac;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Flurl.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nancy;
+using Newtonsoft.Json;
+using Persons.Abstractions;
+using Persons.Abstractions.Commands;
+using Persons.IntegrationTests.Fixtures;
+using Persons.Logging;
+using Persons.Modules;
+using Persons.Service.Dto;
+
+namespace Persons.IntegrationTests.Modules
+{
+    [TestClass]
+    public class PersonsCommandsModuleTests
+    {
+        private PersonsCommandsModule _fixtureModule;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var testsFixture = TestsFixture.RegisterTypes();
+
+            _fixtureModule = new PersonsCommandsModule(testsFixture.Resolve<ICommandHandler<CreatePersonCommand, Guid>>(),testsFixture.Resolve<ILog>());
+        }
+        
+        [TestMethod]
+        public void CreatePerson_CorrectData_NotNull()
+        {
+            var jsonString = JsonConvert.SerializeObject(new
+            {
+                Name = "Name",
+                BirthDay = "1990-09-09"
+            });
+
+            using (var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString)))
+            {
+                var moduleRequest = new Request("POST", new Url("http://someURL"), memoryStream);
+                _fixtureModule.Context = new NancyContext();
+                _fixtureModule.Request = moduleRequest;
+
+                var result = _fixtureModule.CreatePerson();
+
+                Assert.IsNotNull(result);
+            }
+        }
+        
+        [TestMethod]
+        public void CreatePerson_CorrectData_ReturnCreated()
+        {
+            var jsonString = JsonConvert.SerializeObject(new
+            {
+                Name = "Name",
+                BirthDay = "1990-09-09"
+            });
+
+            using (var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString)))
+            {
+                var moduleRequest = new Request("POST", new Url("http://someURL"), memoryStream);
+                _fixtureModule.Context = new NancyContext();
+                _fixtureModule.Request = moduleRequest;
+
+                var result = _fixtureModule.CreatePerson();
+                
+                Assert.AreEqual(HttpStatusCode.Created,result.NegotiationContext.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        public void  CreatePerson_CorrectData_ReturnLocationHeader()
+        {
+            var jsonString = JsonConvert.SerializeObject(new
+            {
+                Name = "Name",
+                BirthDay = "1990-09-09"
+            });
+
+            using (var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString)))
+            {
+                var moduleRequest = new Request("POST", new Url("http://someURL"), memoryStream);
+                _fixtureModule.Context = new NancyContext();
+                _fixtureModule.Request = moduleRequest;
+
+                var result = _fixtureModule.CreatePerson();
+                
+                Assert.IsTrue(result.NegotiationContext.Headers.ContainsKey("Location"));
+            }
+        }
+        
+        [TestMethod]
+        public void CreatePerson_NotJsonData_ReturnBadRequest()
+        {
+            const string data = "wrongString";
+
+            using (var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(data)))
+            {
+                var moduleRequest = new Request("POST", new Url("http://someURL"), memoryStream);
+                _fixtureModule.Context = new NancyContext();
+                _fixtureModule.Request = moduleRequest;
+
+                var result = _fixtureModule.CreatePerson();
+                Assert.AreEqual(HttpStatusCode.BadRequest,result.NegotiationContext.StatusCode);
+            }
+        }
+        
+
+        [DataTestMethod]
+        [DataRow("Name", "1800-09-09")]
+        public void CreatePerson_NotValidData_ReturnUnprocessableEntity(string name, string birthday)
+        {
+            const int unprocessableEntity = 422;
+            
+            var jsonString = JsonConvert.SerializeObject(new
+            {
+                Name = name,
+                BirthDay = birthday
+            });
+            
+            using (var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString)))
+            {
+                var moduleRequest = new Request("POST", new Url("http://someURL"), memoryStream);
+                _fixtureModule.Context = new NancyContext();
+                _fixtureModule.Request = moduleRequest;
+
+                var result = _fixtureModule.CreatePerson();
+                Assert.AreEqual(unprocessableEntity,(int?)result.NegotiationContext.StatusCode);
+            }
+        }
+    }
+}
---
+++ PersonAPI/Persons.IntegrationTests/Modules/PersonsQueriesModuleTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+using Autofac;
+using Flurl;
+using Flurl.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nancy;
+using Nancy.Responses.Negotiation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Persons.Abstractions;
+using Persons.Abstractions.Queries;
+using Persons.IntegrationTests.Fixtures;
+using Persons.Modules;
+using Persons.Service.Dto;
+using Persons.Service.Models;
+using Persons.Service.Repositories;
+
+namespace Persons.IntegrationTests.Modules
+{
+    [TestClass]
+    public class PersonsQueriesModuleTests
+    {
+        private PersonsQueriesModule _fixtureModule;
+        private Guid _newGuid;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var testsFixture = TestsFixture.RegisterTypes();
+            var repository = testsFixture.Resolve<IPersonRepository>();
+            repository.Insert(Person.Create(_newGuid = Guid.NewGuid(), "yrtyrty",DateTime.Today));
+
+            _fixtureModule = new PersonsQueriesModule(testsFixture.Resolve<IQueryHandler<GetPersonQuery, PersonDto>>())
+            {
+                Context = new NancyContext()
+            };
+        }
+        
+        [TestMethod]
+        public void GetPerson_CorrectData_ReturnOk()
+        {
+            var result = _fixtureModule.GetPerson(_newGuid.ToString());
+
+            Assert.AreEqual(HttpStatusCode.OK,result.NegotiationContext.StatusCode);
+        }
+        
+        [TestMethod]
+        public void GetPerson_CorrectData_ReturnNotNullBody()
+        {
+            var result = _fixtureModule.GetPerson(_newGuid.ToString());
+
+            var resultBody = result.NegotiationContext.DefaultModel;
+
+            Assert.IsNotNull(resultBody);
+        }
+
+        [TestMethod]
+        public void GetPerson_NonexistentId_ReturnNotFound()
+        {
+            var result = _fixtureModule.GetPerson(Guid.NewGuid().ToString());
+
+            Assert.AreEqual(HttpStatusCode.NotFound,result.NegotiationContext.StatusCode);
+        }
+
+        [TestMethod]
+        public void GetPerson_IncorrectId_ReturnNotFound()
+        {
+            var result = _fixtureModule.GetPerson("wrongid");
+
+            Assert.AreEqual(HttpStatusCode.NotFound,result.NegotiationContext.StatusCode);
+        }
+    }
+}
